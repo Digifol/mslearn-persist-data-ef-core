ce9143ad6facee42d03bb017ef450bb2c460b28b
Shion Earl-Krogseth <70757955+Digifol@users.noreply.github.com>
2023-09-28 23:55:34 +0200

    First

---
+++ ContosoPizza/Migrations/20230928212550_ModelRevisions.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelRevisions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVegan",
+                table: "Sauces",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVegan",
+                table: "Sauces");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+        }
+    }
+}
